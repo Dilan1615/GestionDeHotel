90af8b3e3be35f0655f432c5b330d88212ff0f3a
Otaka <dilan.chamba@unl.edu.ec>
2024-06-03 22:33:34 -0500

    Mejora_Factura

--- src/Factura.java
+++ src/Factura.java
@@ -1,12 +1,35 @@
 package gestionhotel;
 
+/**
+ *
+ * @author Grupo7PIS
+ */
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 public class Factura {
     private int id;
     private String detalles;
     private LocalDate fecha;
-    private float total;
+    private double costoTotal;
     private Reserva reserva;
+
+    public Factura(int id, String detalles, LocalDate fecha, double costoTotal, Reserva reserva) {
+        this.id = id;
+        this.detalles = detalles;
+        this.fecha = fecha;
+        this.costoTotal = costoTotal;
+        this.reserva = reserva;
+    }
+
+    public void generarFactura() {
+        double costoTotalReserva = reserva.getHabitacion().getPrecio() * 
+                                    ((reserva.getFechaFin().getDayOfYear() - reserva.getFechaInicio().getDayOfYear()) + 1);
+        this.costoTotal = costoTotalReserva + reserva.getCostoServiciosExtras();
+        System.out.println("Factura generada con ID: " + id);
+        System.out.println("Detalles de la factura ID: " + id + ":");
+        System.out.println("Detalles: " + detalles);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Total: " + costoTotal);
+    }
+
 }
