299567de52127bb2da7bc63a0d1a70717644ab22
Isauro Rivera <isauro.rivera@unl.edu.ec>
2024-06-03 23:04:24 -0500

    Clase Persona mejorada

--- src/Persona.java
+++ src/Persona.java
@@ -1,8 +1,32 @@
 package gestionhotel;
 
+/**
+ *
+ * @author Grupo7PIS
+ */
 public class Persona {
     private String nombre;
     private String telefono;
 
+    public Persona(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
 
+    // Getters y setters para los campos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
