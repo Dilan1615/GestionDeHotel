f49361ef246ec73c2046ac6f0e29b8b64297f87a
mechm <maria.chuico@unl.edu.ec>
2024-06-03 22:39:33 -0500

    Mejora_Clase_Huesped
    03/06/2024

--- src/Huesped.java
+++ src/Huesped.java
@@ -1,8 +1,41 @@
 package gestionhotel;
 
+/**
+ *
+ * @author Grupo7PIS
+ */
 public class Huesped {
     private String nombre;
     private String telefono;
-    private int numeroReserva;
+    private int numeroHabitacion;
 
+    public Huesped(String nombre, String telefono, int numeroReserva) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.numeroHabitacion = numeroReserva;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getNumeroReserva() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroReserva(int numeroReserva) {
+        this.numeroHabitacion = numeroReserva;
+    }
 }
