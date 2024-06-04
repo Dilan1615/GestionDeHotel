a4ebc0bd7627fd29115767a0929770793c25fc21
mechm <maria.chuico@unl.edu.ec>
2024-06-03 22:45:02 -0500

    Mejora_Clase_Hotel
    03/06/2024

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,5 +1,9 @@
 package gestionhotel;
 
+/**
+ *
+ * @author Grupo7PIS
+ */
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +12,33 @@ public class Hotel {
     private String direccion;
     private String telefono;
     private String email;
-    private List<Habitacion> habitacionList = new ArrayList<>();
-    private List<Huesped> huespedList = new ArrayList<>();
+    private List<Huesped> huespedes; // Lista de huéspedes
+    private List<Habitacion> habitaciones; // Lista de habitaciones
 
+    public Hotel(String nombre, String direccion, String telefono, String email) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.huespedes = new ArrayList<>(); // Inicializar la lista de huéspedes
+        this.habitaciones = new ArrayList<>(); // Inicializar la lista de habitaciones
+    }
+
+    // Método para agregar un huésped a la lista
+    public void agregarHuesped(Huesped huesped) {
+        huespedes.add(huesped);
+    }
+
+    // Método para agregar una habitación a la lista
+    public void agregarHabitacion(Habitacion habitacion) {
+        habitaciones.add(habitacion);
+    }
+
+    // Método para obtener la lista de habitaciones
+    public List<Habitacion> getHabitacionList() {
+        return habitaciones;
+    }
 }
+
+
+
